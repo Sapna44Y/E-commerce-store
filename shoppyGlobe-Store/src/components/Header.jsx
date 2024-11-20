cb3e87f37480306214c7a4d0eb7612d794931cfb
Sapna44Y <yadavsapna2004@gmail.com>
2024-11-20 23:01:31 +0530

    first commit

---
+++ shoppyGlobe-Store/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./Header.css";
+
+const Header = () => {
+  const categories = ["beauty", "fragrances", "furniture", "groceries"];
+  const cart = useSelector((state) => state.cart);
+  const location = useLocation();
+
+  return (
+    <div>
+      <nav className="navbar">
+        <Link
+          to="/"
+          className={`nav-item ${location.pathname === "/" ? "active" : ""}`}
+        >
+          Home
+        </Link>
+
+        <ul className="categories-list">
+          {categories.map((category) => (
+            <li key={category}>
+              <Link
+                to={`/category/${category.toLowerCase()}`}
+                className={`category-link ${
+                  location.pathname.includes(
+                    `/category/${category.toLowerCase()}`
+                  )
+                    ? "active"
+                    : ""
+                }`}
+              >
+                {category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <Link
+          to="/cart"
+          className={`nav-item ${
+            location.pathname === "/cart" ? "active" : ""
+          }`}
+        >
+          Cart ({cart.length})
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
